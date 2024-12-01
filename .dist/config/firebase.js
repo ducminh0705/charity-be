e97ae46c5a9f54acba436313554da348d54f5545
Minh Do <ducminh0705@gmail.com>
2024-12-01 20:29:22 +0700

    Remove dist directory from Git tracking

--- .dist/config/firebase.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-// import * as admin from 'firebase-admin';
-// import serviceAccount from '../locals/firebaseServiceAccount.json';
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
-// });
-// export default admin;
